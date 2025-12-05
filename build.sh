c33c118fcd994021d631f7407470e24434121cef
Pranav Rao <raopranav@gmail.com>
2025-12-05 16:03:42 -0500

    NO MORE SED OR AWK

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 set -eu
-
+shopt -u patsub_replacement 2>/dev/null || true
 STYLE="tufte.css"
-NAV='<nav><a href="index.html">Home</a> \&nbsp; <a href="projects.html">Projects</a> \&nbsp; <a href="contact.html">Contact</a> \&nbsp; <a href="more.html">More</a></nav><hr>'
+NAV='<nav><a href="index.html">Home</a> &nbsp; <a href="projects.html">Projects</a> &nbsp; <a href="contact.html">Contact</a> &nbsp; <a href="more.html">More</a></nav><hr>'
+HEADER=$(<layout/header.html)
+FOOTER=$(<layout/footer.html)
+HEADER="${HEADER//\{\{NAV\}\}/$NAV}"
+HEADER="${HEADER//\{\{STYLE\}\}/$STYLE}"
 for f in src/*.html; do
-    name=$(basename "$f" .html)
-    title=$(echo "$name" | awk '{print toupper(substr($0,1,1)) substr($0,2)}')
-
-    echo "Building $name..."
-
-    cat layout/header.html "$f" layout/footer.html | \
-    sed "s|{{TITLE}}|$title|g" | \
-    sed "s|{{NAV}}|$NAV|g" | \
-    sed "s|{{STYLE}}|$STYLE|g" > "$name.html"
+    fn="${f##*/}"
+    name="${fn%.*}"
+    title="${name^}"
+    body=$(<"$f")
+    full_page="${HEADER}${body}${FOOTER}"
+    final_html="${full_page//\{\{TITLE\}\}/$title}"
+    printf "%s" "$final_html" > "$name.html"
 done
