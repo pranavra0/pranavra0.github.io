e609803392b73bf95ea2c9886734a06273493bd9
Pranav Rao <raopranav@gmail.com>
2025-12-05 13:44:48 -0500

    Made everything more efficient
    - trimmed down tufte css. Only system fonts, kept same look, much faster now
    - New build.sh - no pandoc dependency, just sed
    - No more markdown. The pages are simple enough to write with html and easier to parse. Allows me to get rid of a lot of logic in the build file as well.

--- build.sh
+++ build.sh
@@ -1,32 +1,15 @@
 set -eu
 
 STYLE="tufte.css"
-NAV='<nav><a href="index.html">Home</a> &nbsp; <a href="projects.html">Projects</a> &nbsp; <a href="contact.html">Contact</a> &nbsp; <a href="more.html">More</a></nav><hr>'
-
-for f in src/*.md; do
-    name=$(basename "$f" .md)
-    title=$(head -n 1 "$f")
-    [ -z "$title" ] && title="$name"
+NAV='<nav><a href="index.html">Home</a> \&nbsp; <a href="projects.html">Projects</a> \&nbsp; <a href="contact.html">Contact</a> \&nbsp; <a href="more.html">More</a></nav><hr>'
+for f in src/*.html; do
+    name=$(basename "$f" .html)
+    title=$(echo "$name" | awk '{print toupper(substr($0,1,1)) substr($0,2)}')
 
     echo "Building $name..."
 
-    cat > "$name.html" <<EOF
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>$name | PRANAV RAO</title>
-    <link rel="stylesheet" href="$STYLE">
-</head>
-<body>
-    <header>$NAV</header>
-    <article>
-EOF
-    pandoc "$f" -f markdown -t html >> "$name.html"
-    cat >> "$name.html" <<EOF
-    </article>
-</body>
-</html>
-EOF
+    cat layout/header.html "$f" layout/footer.html | \
+    sed "s|{{TITLE}}|$title|g" | \
+    sed "s|{{NAV}}|$NAV|g" | \
+    sed "s|{{STYLE}}|$STYLE|g" > "$name.html"
 done
--- src/more.md
+++
@@ -1,7 +0,0 @@
-Various projects and things that I made for myself / for fun 
-
-- [Boids](https://pranavra0.github.io/boids/) - Flock Simulation
-
-- [EOPStop](https://pranavra0.github.io/other/eopstop/index.html) - A browser based subtitle syncing tool 
-
-- [Embedst](https://embedst.vercel.app/) - A stateless Typst embed generator for social platforms  
